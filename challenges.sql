9820a5a87dedbc19b62338618a6c9f2f5626cc60
MigPalSer <migpalser@protonmail.ch>
2021-07-27 13:00:04 +0200

    Create challenges.sql

---
+++ challenges.sql
@@ -0,0 +1,4 @@
+--Challenges from hackerrank.com for SQL. I use the MySQL syntaxis.
+
+--https://www.hackerrank.com/challenges/revising-the-select-query/problem
+SELECT * FROM CITY WHERE POPULATION > 100000 AND COUNTRYCODE="USA";
