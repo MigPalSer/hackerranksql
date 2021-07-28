dc25b8a65ffa9c7cd51994ed409b7f346a23a07f
MigPalSer <migpalser@protonmail.ch>
2021-07-28 11:54:17 +0200

    Update challenges.sql

--- challenges.sql
+++ challenges.sql
@@ -5,3 +5,6 @@ SELECT * FROM CITY WHERE POPULATION > 100000 AND COUNTRYCODE="USA";
 
 --https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
 SELECT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCODE="USA";
+
+--https://www.hackerrank.com/challenges/select-all-sql/problem
+SELECT * FROM CITY;
