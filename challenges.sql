05173f64431c8829ae6bb6b7e08873b869f6a5b5
MigPalSer <migpalser@protonmail.ch>
2021-07-29 09:35:23 +0200

    Update challenges.sql

--- challenges.sql
+++ challenges.sql
@@ -8,3 +8,6 @@ SELECT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCODE="USA";
 
 --https://www.hackerrank.com/challenges/select-all-sql/problem
 SELECT * FROM CITY;
+
+--https://www.hackerrank.com/challenges/select-by-id/problem
+SELECT * FROM CITY WHERE ID=1661;
