f74df8045b7938bd3e94629ecb1ec4add1df9f9b
MigPalSer <migpalser@protonmail.ch>
2021-07-29 09:37:03 +0200

    Update challenges.sql

--- challenges.sql
+++ challenges.sql
@@ -11,3 +11,6 @@ SELECT * FROM CITY;
 
 --https://www.hackerrank.com/challenges/select-by-id/problem
 SELECT * FROM CITY WHERE ID=1661;
+
+--https://www.hackerrank.com/challenges/japanese-cities-attributes/
+SELECT * FROM CITY WHERE COUNTRYCODE="JPN";
