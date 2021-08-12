b12bb8c5dc64b53e32421fe802216c887bc55e0b
MigPalSer <migpalser@protonmail.ch>
2021-08-12 15:04:17 +0200

    Update challenges.sql

--- challenges.sql
+++ challenges.sql
@@ -45,3 +45,6 @@ SELECT DISTINCT CITY FROM STATION WHERE CITY RLIKE '[aeiou]$';
 
 --https://www.hackerrank.com/challenges/weather-observation-station-9/problem
 SELECT DISTINCT CITY FROM STATION WHERE CITY RLIKE '^[^AEIOU]';
+
+--https://www.hackerrank.com/challenges/weather-observation-station-10/problem
+SELECT DISTINCT CITY FROM STATION WHERE CITY RLIKE '[^aeiou]$';
