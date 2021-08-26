65466880bf45038cc407524187b9f1e9b93e2642
MigPalSer <migpalser@protonmail.ch>
2021-08-26 08:52:29 +0200

    Update challenges.sql

--- challenges.sql
+++ challenges.sql
@@ -92,3 +92,6 @@ SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT='California';
 
 --https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
 SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT='California';
+
+--https://www.hackerrank.com/challenges/average-population/problem
+SELECT ROUND(AVG(POPULATION)) FROM CITY;
