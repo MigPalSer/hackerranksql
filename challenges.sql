67d6ff22b4a1a13f64cde7f33f74ca8b47dfaae0
MigPalSer <migpalser@protonmail.ch>
2021-09-22 08:41:01 +0200

    Update challenges.sql

--- challenges.sql
+++ challenges.sql
@@ -122,3 +122,5 @@ SELECT ROUND(LONG_W, 4) FROM STATION WHERE LAT_N<137.2345 ORDER BY LAT_N DESC LI
 
 --https://www.hackerrank.com/challenges/weather-observation-station-16/problem
 SELECT ROUND(LAT_N, 4) FROM STATION WHERE LAT_N>38.7780 ORDER BY LAT_N ASC LIMIT 1;
+
+--https://www.hackerrank.com/challenges/weather-observation-station-17/problem
