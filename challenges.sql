4443a1ce7fac1c1a4dc628571d449891f1b70e1f
MigPalSer <migpalser@protonmail.ch>
2021-10-09 10:25:38 +0200

    Update challenges.sql

--- challenges.sql
+++ challenges.sql
@@ -135,4 +135,4 @@ SELECT ROUND(ABS(MAX(LAT_N)-MIN(LAT_N))+ABS(MAX(LONG_W)-MIN(LONG_W)),4) FROM STA
 --https://www.hackerrank.com/challenges/weather-observation-station-19/problem
 SELECT ROUND(SQRT(POWER(MAX(LAT_N)-MIN(LAT_N),2)+POWER(MAX(LONG_W)-MIN(LONG_W),2)),4) FROM STATION;
 
---
+--https://www.hackerrank.com/challenges/asian-population/problem
