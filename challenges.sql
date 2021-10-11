c25fab95f383f480e304d876a19d5038c6f1c46a
MigPalSer <migpalser@protonmail.ch>
2021-10-11 09:12:41 +0200

    Update challenges.sql

--- challenges.sql
+++ challenges.sql
@@ -136,3 +136,5 @@ SELECT ROUND(ABS(MAX(LAT_N)-MIN(LAT_N))+ABS(MAX(LONG_W)-MIN(LONG_W)),4) FROM STA
 SELECT ROUND(SQRT(POWER(MAX(LAT_N)-MIN(LAT_N),2)+POWER(MAX(LONG_W)-MIN(LONG_W),2)),4) FROM STATION;
 
 --https://www.hackerrank.com/challenges/asian-population/problem
+
+--
